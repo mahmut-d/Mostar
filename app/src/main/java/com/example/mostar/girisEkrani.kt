5572b54d857e71ea0038936d6853f0d858a53e54
Mahmut DOĞAN <mahmut_d@outlook.com>
2022-12-15 05:03:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mostar/girisEkrani.kt
@@ -0,0 +1,13 @@
+package com.example.mostar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class girisEkrani : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_giris_ekrani)
+    }
+
+
+}
